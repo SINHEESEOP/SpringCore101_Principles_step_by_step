3659908a797f437a061eac6585eca8fb55580ffa
SINHEESEOP <shfurwnd97@gmail.com>
2025-03-20 19:00:17 +0900

    refactor: 스프링 빈 상속 관계 조회 테스트 코드 개선

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindsTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindsTest.java
@@ -10,17 +10,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationContextExtendsFindsTest {
 
-    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
 
     @Test
     @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 중복 오류가 발생한다")
     void findBeanByParentTypeDuplicate() {
-        DiscountPolicy bean = context.getBean(DiscountPolicy.class);
+//        DiscountPolicy bean = context.getBean(DiscountPolicy.class);
         assertThrows(NoUniqueBeanDefinitionException.class,
                 () -> context.getBean(DiscountPolicy.class));
     }
@@ -39,6 +41,24 @@ public class ApplicationContextExtendsFindsTest {
         assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
     }
 
+    @Test
+    @DisplayName("부모 타입으로 조회하기")
+    void findBeanByParentType() {
+        Map<String, DiscountPolicy> beanTypes = context.getBeansOfType(DiscountPolicy.class);
+        assertThat(beanTypes.size()).isEqualTo(2);
+        for (String key : beanTypes.keySet()) {
+            System.out.println("Key : " + key + ", value : " + beanTypes.get(key));
+        }
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기 - Object")
+    void findBeanByObjectType() {
+        Map<String, Object> beansOfType = context.getBeansOfType(Object.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("Key : " + key + ", value : " + beansOfType.get(key));
+        }
+    }
 
     @Configuration
     static class TestConfig {
